92dc260c0f828d44171a00cff783724645d35231
Benoit Manhes <benoit.manhes.82@gmail.com>
2018-12-05 14:59:00 -0500

    panel_profil_visiteur

--- src/Controller/ControllerProfil.java
+++ src/Controller/ControllerProfil.java
@@ -1,12 +1,21 @@
 package Controller;
 
+import javax.swing.JPanel;
+
 import View.FrameMain;
+import View.PanelEmprunt;
+import View.PanelEmpruntUtilisateur;
 import View.PanelProfil;
+import View.PanelProfilVisiteur;
+import user.Droit;
 
 public class ControllerProfil{
 
 	 public ControllerProfil(ControllerManager controller, FrameMain frame) {
-	        PanelProfil p = new PanelProfil();
+
+		JPanel p = new JPanel();
+		if(controller.utilisateur.getDroit().equals(Droit.visiteur)) p = new PanelProfilVisiteur();
+		else p = new PanelProfil();
 
 	        frame.getDesktopContainer().add(p);
 	        frame.getDesktopContainer().updateUI();
